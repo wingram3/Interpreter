d64f2e77535fa28a5fe1450d34cf2d596522dbd4
Will Ingram <mingmangram@gmail.com>
2024-06-08 16:59:42 -0700

    Visitor

--- Part-I/com/tool/GenerateAst.java
+++ Part-I/com/tool/GenerateAst.java
@@ -41,14 +41,45 @@ public class GenerateAst {
         writer.println("abstract class " + baseName + " {");
         writer.println();
 
+        defineVisitor(writer, baseName, types);
+
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
 
+        // the base accept() method.
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(
+        PrintWriter writer,
+        String baseName,
+        List<String> types
+    ) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println(
+                "        R visit" +
+                typeName +
+                baseName +
+                "(" +
+                typeName +
+                " " +
+                baseName.toLowerCase() +
+                ");"
+            );
+        }
+
+        writer.println("    }");
+        writer.println();
     }
 
     private static void defineType(
@@ -69,6 +100,18 @@ public class GenerateAst {
             writer.println("            this." + name + " = " + name + ";");
         }
 
+        writer.println("        }");
+
+        // Visitor pattern.
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println(
+            "            return visitor.visit" +
+            className +
+            baseName +
+            "(this);"
+        );
         writer.println("        }");
 
         writer.println();
